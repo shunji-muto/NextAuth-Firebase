6c430c87cad75208fd9a6f5f27689b6492f82e14
shunji-muto <46043453+shunji-muto@users.noreply.github.com>
2025-01-25 03:16:21 +0000

    保護されたページにユーザー名を表示する機能を追加

--- next-auth_firebase-auth/src/app/(protected)/protected-page/page.tsx
+++ next-auth_firebase-auth/src/app/(protected)/protected-page/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 
 import { logOutWithFirebaseAuth } from "@/lib/firebase/firebase-auth"
+import { useSession } from "next-auth/react";
 import Link from "next/link"
 
 const Page = () => {
+  const {data:session} = useSession();
+
   return (
     <div style={{display:"flex", flexDirection:"column", gap: 20}}>  
         <div style={{fontSize: 20, fontWeight: 20}}>ログインした人限定のページ</div>
+        <div>あなたの名前は{session?.user?.name}さんです</div>
         <div style={{display:"flex", flexDirection:"column"}}>
             <button>
                 <Link href={`/`}>"/"ページへのリンク</Link>
