03e1528dcedf92274df9a3d7c8108ed3512bf534
shunji-muto <46043453+shunji-muto@users.noreply.github.com>
2025-01-25 02:44:51 +0000

    Firebase Admin SDKの環境変数をNEXT_PUBLICプレフィックスに変更

--- next-auth_firebase-auth/src/lib/firebase/firebase-admin.ts
+++ next-auth_firebase-auth/src/lib/firebase/firebase-admin.ts
@@ -2,9 +2,9 @@ import * as admin from "firebase-admin";
 import type { ServiceAccount } from "firebase-admin";
 
 const cert: ServiceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
 const firebaseAdmin =
