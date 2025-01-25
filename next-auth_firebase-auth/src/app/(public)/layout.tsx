199789dc68a44145a78eafffca9172d9c29289f8
shunji-muto <46043453+shunji-muto@users.noreply.github.com>
2025-01-25 02:44:31 +0000

    レイアウトコンポーネントを追加し、ユーザーセッションに基づいてリダイレクトを実装

---
+++ next-auth_firebase-auth/src/app/(public)/layout.tsx
@@ -0,0 +1,15 @@
+import { authOptions } from "@/lib/firebase/auth"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+import { ReactElement } from "react"
+
+const Layout = async ({children}:{children:ReactElement}) => {
+  const session = await getServerSession(authOptions)
+
+  if(session?.user) redirect(`/protected-page`)
+
+  return <>{children}</>
+  
+}
+
+export default Layout
