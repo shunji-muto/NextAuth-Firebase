ed04d7665e3c967a4ceeac22d0d2bc24dce884b5
shunji-muto <46043453+shunji-muto@users.noreply.github.com>
2025-01-25 06:43:00 +0000

    Firebase認証の環境変数を更新し、クライアントメールとプライベートキーの取得方法を修正

--- next-auth_firebase-auth/src/lib/firebase/firebase-admin.ts
+++ next-auth_firebase-auth/src/lib/firebase/firebase-admin.ts
@@ -3,8 +3,8 @@ import type { ServiceAccount } from "firebase-admin";
 
 const cert: ServiceAccount = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
 const firebaseAdmin =
