2bd4b3bf5e6096a9eea5e9e8dca43c262b13bfeb
shunji-muto <46043453+shunji-muto@users.noreply.github.com>
2025-01-25 02:43:34 +0000

    NextAuthのコア部分の実装

---
+++ next-auth_firebase-auth/src/lib/firebase/auth.ts
@@ -0,0 +1,101 @@
+import type { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { firebaseAdminAuth } from './firebase-admin'
+
+const fetchNewIdToken = async (refreshToken: string) => {
+  const res = await fetch(
+    `https://securetoken.googleapis.com/v1/token?key=${process.env.FIREBASE_TOKEN_API_KEY}`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        grant_type: 'refresh_token',
+        refreshToken,
+      }),
+    },
+  )
+
+  const { id_token } = await res.json()
+
+  return id_token
+}
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      credentials: {},
+      // 1
+      authorize: async ({ idToken, refreshToken }) => {
+        if (idToken && refreshToken) {
+          try {
+            const decoded = await firebaseAdminAuth.verifyIdToken(idToken) // 2
+
+            const user = {
+              id: decoded.user_id,
+              uid: decoded.uid,
+              name: decoded.name || '',
+              email: decoded.email || '',
+              image: decoded.picture || '',
+              emailVerified: decoded.email_verified || false,
+              idToken,
+              refreshToken,
+              tokenExpiryTime: decoded.exp || 0,
+            }
+
+            return user
+          } catch (err) {
+            console.error(err)
+          }
+        }
+        return null
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+        token.uid = user.id
+        token.name = user.name ?? ''
+        token.emailVerified = !!user.emailVerified
+        token.idToken = user.idToken
+        token.refreshToken = user.refreshToken
+        token.image = user.image ?? ''
+        token.tokenExpiryTime = user.tokenExpiryTime
+      }
+
+      const currentTime = Math.floor(Date.now() / 1000)
+      const tokenExpiryTime = token.tokenExpiryTime as number
+      const isExpired = currentTime > tokenExpiryTime - 300 // 5分前には更新するようにする
+
+      if (isExpired) {
+        try {
+          const newIdToken = await fetchNewIdToken(token.refreshToken as string)
+          token.idToken = newIdToken
+        } catch (error) {
+          console.error('Error refreshing token:', error)
+        }
+      }
+
+      return token
+    },
+    async session({ session, token }) {
+      // sessionにFirebase Authenticationで取得した情報を追加。
+      session.user.emailVerified = token.emailVerified
+      session.user.uid = token.uid
+      session.user.name = token.name
+      session.user.image = token.image || ''
+      session.idToken = token.idToken || ''
+      session.user.birth = userData.birth || '' // データベースから取得した値もsession情報に追加
+
+      return session
+    },
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 90 * 24 * 60 * 60, // 90 days
+  },
+  pages: {
+    signIn: '/sign-in',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+}
