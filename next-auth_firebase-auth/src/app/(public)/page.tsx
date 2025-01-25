8f0972723aa278d71ee0f76b7a02c260e677ce58
shunji-muto <46043453+shunji-muto@users.noreply.github.com>
2025-01-25 03:04:14 +0000

    ログインページに保護されたページへのリンクを追加

--- next-auth_firebase-auth/src/app/(public)/page.tsx
+++ next-auth_firebase-auth/src/app/(public)/page.tsx
@@ -1,13 +1,19 @@
 "use client"
 
 import { logInWithFirebaseAuth } from "@/lib/firebase/firebase-auth"
+import Link from "next/link"
 
 const Page = () => {
   return (
-    <div style={{display:"flex", flexDirection:"column"}}>
+    <div style={{display:"flex", flexDirection:"column", gap: 20}}>
         <div style={{fontSize:20, fontWeight:20}}>
             ログインページです
         </div>
+        <Link href={`/protected-page`}>
+          <button>
+              "/protected-page"ページへのリンク（※ログインしてないため、このページに戻ってくるよ）
+          </button>
+        </Link>
         <button onClick={logInWithFirebaseAuth} style={{height:"48px", padding: "8px"}}>
             Googleでログインする
         </button>
